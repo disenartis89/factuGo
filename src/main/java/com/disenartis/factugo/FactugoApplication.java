ca85469f1fbecd5d5745098163803713a53a0f90
Raquel <raquelcferreiro@gmail.com>
2026-05-09 22:40:49 +0200

    Initial commit - estructura base Spring Boot

---
+++ src/main/java/com/disenartis/factugo/FactugoApplication.java
@@ -0,0 +1,13 @@
+package com.disenartis.factugo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactugoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactugoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/disenartis/factugo/FactugoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.disenartis.factugo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactugoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
